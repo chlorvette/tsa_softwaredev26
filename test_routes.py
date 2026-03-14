052b7326ba726b27ce335001e540098029d4011d
anya-codes <anya.rajyaguru@gmail.com>
2026-03-13 23:18:47 -0700

    test all routes to confirm they return 200 using pytest

---
+++ test_routes.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_all_routes(client):
+    urls = ['/', '/my-courses', '/course/1/', '/course/2/', '/course/3/', '/course/4/', '/login', '/help', '/register']
+    
+    for url in urls:
+        response = client.get(url)
+        assert response.status_code == 200, f"Failed: {url}"
+        
